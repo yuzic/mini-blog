736423f4fa15e20f17e81fe57264453269c6978b
itcoder <prototypenk@gmail.com>
2016-04-30 16:29:16 +0300

    add test

--- tests/BlogModelTest.php
+++ tests/BlogModelTest.php
@@ -10,4 +10,20 @@ class MoneyTest extends PHPUnit_Framework_TestCase
         $this->assertArrayHasKey('count', $model->getCount());
     }
 
+
+    public function testSave()
+    {
+        $params = [
+            'title' => 'title',
+            'text' => 'text',
+            'image_path' => '/Image/image.jpg',
+            'created_at' => time(),
+            'modify_at' => time(),
+        ];
+
+        $model = new Blog();
+        ;
+        $this->assertTrue($model->save($params));
+    }
+
 }
